f5a41457827fe69d62f903e621ac020e02b76157
fuzh <l.l.zhonghua@163.com>
2012-10-16 14:44:26 +0800

    globle layout

--- app/assets/javascripts/fastui/app/view/mwindow/List.js
+++ app/assets/javascripts/fastui/app/view/mwindow/List.js
@@ -14,14 +14,6 @@ Ext.define('FastUI.view.mwindow.List', {
 //    store: 'Users',
     store: 'MWindows',
     initComponent: function () {
-//        this.store = {
-//            fields: ['name', 'email'],
-//            data  : [
-//                {name: 'Ed',    email: 'ed@sencha.com'},
-//                {name: 'Tommy', email: 'tommy@sencha.com'}
-//            ]
-//        };
-
         this.columns = [
             {header: 'Name',  dataIndex: 'name',  flex: 1},
             {header: 'Email', dataIndex: 'email', flex: 1}
