59cee8f7f29ece94a22213655ed47fd5dbae1eb2
msj <msj@chunteng.net>
2012-10-19 09:02:59 +0800

    111

--- app/models/fastui/m_window.rb
+++ app/models/fastui/m_window.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 module Fastui
   class MWindow < ActiveRecord::Base
-    attr_accessible :note, :title
+    attr_accessible :note, :title, :help, :active, :org_id, :createdby, :updatedby
     has_many :m_tabs, :dependent => :destroy
   end
 end
