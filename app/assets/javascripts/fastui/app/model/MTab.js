b8617e2b359001ef8fb83168c2c14075872b83ab
fuzh <l.l.zhonghua@163.com>
2012-10-23 13:39:12 +0800

    scope :this

--- app/assets/javascripts/fastui/app/model/MTab.js
+++ app/assets/javascripts/fastui/app/model/MTab.js
@@ -1,16 +1,17 @@
 Ext.define('FastUI.model.MTab', {
     extend: 'Ext.data.Model',
     fields: ['id','title','model_class','note','help','actived','org_id','m_window_id'],
-    associations: [
-        {
-            type: 'belongsTo',
-            model: 'FastUI.model.MWindow',
-            primaryKey: 'id',
-            foreignKey: 'm_window_id',
-            autoLoad: true,
-            name:'m_window'
-        }
-    ],
+//    associations: [
+//        {
+//            type: 'belongsTo',
+//            model: 'FastUI.model.MWindow',
+//            primaryKey: 'id',
+//            foreignKey: 'm_window_id',
+//            autoLoad: true,
+//            name:'m_window'
+//        }
+//    ],
+    belongsTo: 'FastUI.model.MWindow',
     proxy: {
         type: 'rest',
         url : '/fastui/m_windows/:id/m_tabs',
