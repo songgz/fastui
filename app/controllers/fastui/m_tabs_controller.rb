6d1a349d302c9dd312da163ed43ab1b290b44a7d
fuzh <l.l.zhonghua@163.com>
2012-10-23 13:19:12 +0800

    scope :this

---
+++ app/controllers/fastui/m_tabs_controller.rb
@@ -0,0 +1,16 @@
+require_dependency "fastui/application_controller"
+
+module Fastui
+  class MTabsController < ApplicationController
+    respond_to :html,:xml,:json
+    def index
+      @m_tabs = MTab.all
+      respond_with(@m_tabs)
+    end
+
+    def show
+      @m_tab = MTab.find(params[:id])
+      respond_with(@m_tab)
+    end
+  end
+end
