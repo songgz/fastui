932974b6c7c2cde5c64f5405570a93cb9b204d3a
fuzh <l.l.zhonghua@163.com>
2012-10-30 17:00:48 +0800

    use mtabs each

--- app/assets/javascripts/fastui/app/view/VGrid.js
+++ app/assets/javascripts/fastui/app/view/VGrid.js
@@ -1,6 +1,8 @@
 
 Ext.define('FastUI.view.VGrid', {
     extend: 'Ext.grid.Panel',
+    selType: 'rowmodel',
+    multiSelect:false,
     initComponent:function(){
         this.columns = this.vfactory.getColumns();
         this.store = this.vfactory.getStore();
