58513b90939f5ca261a9ab53cf98654c73ca28a7
songgz <sgzhe@163.com>
2012-11-13 15:44:13 +0800

    Merge branch 'master' of 192.168.0.202:fastui
    Conflicts:
    	app/models/fastui/m_column.rb
    	app/models/fastui/m_data_type.rb
    	app/models/fastui/m_entity.rb
    	app/models/fastui/m_field.rb
    	app/models/fastui/m_glossary.rb
    	app/models/fastui/m_glossary_item.rb
    	app/models/fastui/m_property.rb
    	app/models/fastui/m_tab.rb
    	app/models/fastui/m_window.rb
    	db/migrate/20121002004450_create_fastui_m_entities.rb
    	db/migrate/20121002004602_create_fastui_m_properties.rb
    	db/migrate/20121002073331_create_fastui_m_glossaries.rb
    	db/migrate/20121002074436_create_fastui_m_glossary_items.rb
    	db/migrate/20121005052006_create_fastui_m_data_types.rb
    	db/migrate/20121016002755_create_fastui_m_windows.rb
    	db/migrate/20121016013337_create_fastui_m_tabs.rb
    	db/migrate/20121016013608_create_fastui_m_fields.rb
    	db/migrate/20121016013731_create_fastui_m_columns.rb
    	lib/tasks/fastui_tasks.rake

--- app/assets/javascripts/fastui/app/view/VGrid.js
+++ app/assets/javascripts/fastui/app/view/VGrid.js
@@ -4,6 +4,7 @@ Ext.define('FastUI.view.VGrid', {
     selType: 'rowmodel',
     multiSelect:false,
     initComponent:function(){
+        this.title = this.vfactory.getVData().title;
         this.columns = this.vfactory.getColumns();
         this.store = this.vfactory.getStore();
         this.callParent();
