ff3f9cb52e2d22c8df645fa8a69d242f596a2b79
fuzh <l.l.zhonghua@163.com>
2012-12-12 13:21:50 +0800

    modify actived and  id to string

--- app/models/fastui/m_property.rb
+++ app/models/fastui/m_property.rb
@@ -6,10 +6,6 @@ module Fastui
 
     belongs_to :m_entity, :class_name => 'Fastui::MEntity'
     belongs_to :m_datatype,:class_name => 'Fastui::MDatatype'
-    #belongs_to :refable, :polymorphic => true
-
-    #belongs_to :entity_kind, :class_name => 'Fastui::MGlossaryItem'     :entity_kind_id,
-    #belongs_to :actived, :class_name => 'Fastui::MGlossaryItem'
     belongs_to :org, :class_name => 'Fastui::MOrg'
     belongs_to :createdby, :class_name => 'Fastui::MPerson'
     belongs_to :updatedby, :class_name => 'Fastui::MPerson'
