465f1a5e719f6c7a8606294565fb9191624e069b
fuzh <l.l.zhonghua@163.com>
2013-01-16 10:00:46 +0800

    break down MDatatype file class  and add createdbyorg_id to all of entities

--- db/migrate/20121017025014_create_fastui_m_people.rb
+++ db/migrate/20121017025014_create_fastui_m_people.rb
@@ -3,12 +3,15 @@ class CreateFastuiMPeople < ActiveRecord::Migration
     create_table :fastui_m_people do |t|
       t.string :title, :limit => 60, :null => false
       t.string :print_text,:default => '',:limit => 60
+      t.string :note , :default => ''
       t.string :name,:default => '',:limit => 60
 
+      t.integer :seq, :default => 0
       t.boolean :is_active, :default => true
       t.integer :createdbyorg_id, :default => 0
       t.integer :createdby_id, :default => 0
       t.integer :updatedby_id, :default => 0
+      t.string :help, :default => ''
 
       t.timestamps
     end
