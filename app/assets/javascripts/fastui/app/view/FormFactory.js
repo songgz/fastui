5e7159412b71c6da394935458b523bdc8bfa1502
fzh <fzh@githum.com>
2013-05-09 13:34:58 +0800

    Model.all modify Model.scoped

--- app/assets/javascripts/fastui/app/view/FormFactory.js
+++ app/assets/javascripts/fastui/app/view/FormFactory.js
@@ -5,7 +5,7 @@ Ext.define('FastUI.view.FormFactory', {
         if (form_kind == '') {
             return Ext.create('FastUI.view.VForm', {tab: tab});
         } else {
-            return Ext.create('FastUI.view.' + this.form_kind, {tab: tab});
+            return Ext.create('FastUI.view.' + form_kind, {tab: tab});
         }
     }
 });
